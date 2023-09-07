a8171f72253257253b16f22d46f16ab81c6db891
Sarona19 <sarrooadel.99@gmail.com>
2023-09-07 14:49:57 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,12 @@
 #include<stdio.h>
 /**
-*main -A program is prints aline with puts function
-*return: 0 (success)
-*/
+ * main -A program is prints aline with puts functio
+n
+ * Return: 0 (success)
+ */
 int main(void)
 {
-puts("Programming is like building a multilingual puzzle/n");
-return 0;
+puts("\"Programming is like building a multilingual puzz
+le");
+return (0);
 }
