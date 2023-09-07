9bb8a7c2a51c0f1430dff0614eba2cb1ad78986b
Sarona19 <sarrooadel.99@gmail.com>
2023-09-07 14:37:32 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,10 @@
-#!/bin/bash               
 #include<stdio.h>
-int main() {
+/**
+ * main -A program is prints aline with puts function
+ * return: 0 (success)
+ */
+int main(void)
+{
 	puts("Programming is like building a multilingual puzzle/n");
   return 0; 
 }
