dd093681f3957bf588d5d464479dab2c8c42c83e
Sarona19 <sarrooadel.99@gmail.com>
2023-09-16 10:29:35 -0700

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -4,7 +4,7 @@
 
 /**
  * main - print the last digit of the number stored in the variable n
- * n is greater than 5 ,n is 0 , n is less than 6 and not 0
+ *
  * Return: always 0 (success)
  **/
 int main(void)
