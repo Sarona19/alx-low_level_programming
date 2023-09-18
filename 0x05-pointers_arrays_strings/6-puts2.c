aeb41e789cb16b8098e57926adf0500c8938dab4
Sarona19 <sarrooadel.99@gmail.com>
2023-09-18 15:32:32 -0700

    6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,10 +1,9 @@
 #include "main.h"
 
 /**
- * rev_string - prints every other character of a string,
- * starting with the first character, followed by a new line.
+ * rev_string - prints character of a string.
  *
- * @s: string parameter input
+ * @str: string parameter input
  *
  * Return: Nothing
  **/
@@ -13,10 +12,11 @@ void rev_string(char *str)
 {
 	int i;
 
-	for (i = 0;' str[i] != '\0'; ++i)
+	for (i = 0; str[i] != '\0'; ++i)
 	{
-		ifn (i % 2 ==0)
-			putchar(str[i]);
+		if (i % 2 == 0)
+
+		putchar(str[i]);
 	}
 
 	putchar('\n');
