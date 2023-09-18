2ae95b939d1db015c3b58f2451783d082a962cf6
Sarona19 <sarrooadel.99@gmail.com>
2023-09-18 13:46:11 -0700

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ *
+ * _strlen -function that returns the length of a string.
+ *
+ * @s: string parameter input
+ *
+ * Return: Nothing
+ **/
+
+int _strlen(char *s)
+{
+	int i;
+	for (i = 0; *s != "/0"; s++)
+		++i;
+	return (i);
+}
