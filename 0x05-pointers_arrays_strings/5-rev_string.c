0c43b1d14fbe5ccd38cbbd76e4716359e0534baa
Sarona19 <sarrooadel.99@gmail.com>
2023-09-18 14:44:19 -0700

    5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * rev_string - reverses a string.
+ *
+ * @s: string parameter input
+ *
+ * return: Nothing
+ **/
+
+void rev_string(char *s)
+{
+	int l, i;
+	char temb;
+
+	for (l = 0; s[l] != '\0'; ++l);
+
+	for (i = 0; i < 1 / 2; i++)
+		temb = s[i];
+		s[i] = s[l - 1 - i];
+		s[l - 1 - i] = temb;
+}
