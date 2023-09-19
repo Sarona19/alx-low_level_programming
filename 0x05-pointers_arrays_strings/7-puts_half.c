9d9fb7b7e2e7e7f41189e2bc11370513539fa698
Sarona19 <sarrooadel.99@gmail.com>
2023-09-19 15:27:03 -0700

    7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -6,7 +6,7 @@
  * @str: char array string type
  *
  * Description: it odd number of chars, print[length - 1] / 2
- * 
+ *
  **/
 
 void puts_half(char *str)
@@ -14,7 +14,6 @@ void puts_half(char *str)
 	int i;
 
 	for (i = 0; str[i] != '\0'; i++)
-	;
 		i++;
 	for (i /= 2; str[i] != '\n'; i++)
 	{
